c39789c9c2f3658e021b9ab46d70ebf5bc003c89
fmesa-or <franciscomesaempleo@gmail.com>
2024-10-01 16:36:21 +0200

    Organizado

--- include/minishell.h
+++ include/minishell.h
@@ -1,14 +1,14 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oceanscorelive <oceanscorelive@student.    +#+  +:+       +#+        */
+/*   By: rmarin-j <rmarin-j@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 16:58:52 by rmarin-j          #+#    #+#             */
-/*   Updated: 2024/09/30 20:35:46 by oceanscorel      ###   ########.fr       */
+/*   Updated: 2024/10/01 14:15:04 by rmarin-j         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
@@ -32,6 +32,12 @@ typedef struct s_list
 char	**ft_split(char const *s, char c);
 int		ft_strlen(const char *str);
 /*-----------strjoin-----------*/
+int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcjoin(char const *s1, char const *s2, char c);
+
+/*----------list_utils----------*/
+t_list  *ft_unset(t_list *list, char *ref);
+char **listtoenv(t_list *list);
+t_list  *envtolist(char **env);
 
 #endif
--- src/utils/ft_strcjoin.c
+++ src/utils/ft_strcjoin.c
@@ -6,11 +6,26 @@
 /*   By: rmarin-j <rmarin-j@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 18:46:12 by rmarin-j          #+#    #+#             */
-/*   Updated: 2024/09/30 19:27:43 by rmarin-j         ###   ########.fr       */
+/*   Updated: 2024/10/01 14:11:42 by rmarin-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
+
 
 char	*cjoin(char *s1, char *s2, char *ptr, char c)
 {
