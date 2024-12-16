87c4c88b4b5323742b26b46574e385d2a475b86a
fmesa-or <152217033+fmesa-or@users.noreply.github.com>
2024-12-16 14:41:02 +0100

    executer & commander

--- src/paco/execute.c
+++ src/paco/execute.c
@@ -6,21 +6,84 @@
 /*   By: fmesa-or <fmesa-or@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 21:35:00 by fmesa-or          #+#    #+#             */
-/*   Updated: 2024/12/16 13:08:08 by fmesa-or         ###   ########.fr       */
+/*   Updated: 2024/12/16 14:40:41 by fmesa-or         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.c"
 
-void	ft_execute(t_token token)
+void	ft_comander(t_token *token, t_data *data)
+{
+	//esto es el piex
+	//pid está en data
+	//token->redir -> type = IN || file = "input.txt" || next= NULL
+	//token->argv -> [0]= ls; [1]=-l; [2]=-a; [3]=NULL
+
+	//ultimo input y ultimo output
+
+
+
+	if (token->type == HDOC)
+	{
+		
+	}
+	else
+	{
+		filout = tin_opener();
+		filein = tin_opener();
+		dup2(filein, STDIN_FILENO);
+	}
+
+
+/*
+	int	i;
+	int	fileout;
+	int	filein;
+
+	if (ft_strncmp(argv[1], "here_doc", 8) == 0)
+	{
+		i = 3;
+		fileout = tin_opener(argv[argc - 1], 0);
+		here_doc(argv[2], argc);
+	}
+	else
+	{
+		i = 2;
+		fileout = tin_opener(argv[argc - 1], 1);
+		filein = tin_opener(argv[1], 2);
+		dup2(filein, STDIN_FILENO);
+	}
+	while (i < (argc - 2))
+		child_process_bonus(argv[i++], envp);
+	dup2(fileout, STDOUT_FILENO);
+	ft_execute(argv[argc - 2], envp);
+	wait(NULL);
+*/
+
+
+
+}
+
+void	ft_builtin(char *command)
+{
+	if export
+	if unset
+	if cd
+	if pwd
+	if env
+	if echo
+}
+
+void	ft_execute(t_token token, t_data data)
 {
 
-	if (token->cmmd == "exit")
-		exit.
-	if (built in)
-		built in.
-	if (cmd)
-		cmd.
+	if (token->command == "exit")
+		ft_exit(token->argv);
+	if (token->type == BUIL)
+		ft_builtin(token->command);
+	if (token->type == CMD)
+		ft_comander(token, data);
+
 	//1-Existe un comando después? -> Hacer una pipe. Modificando el fd.
 	
 
