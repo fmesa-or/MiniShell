3a99a44e041ac4be981ee43aabc8b98b9a425e3b
rmarin-j <rmarin-j@student.42malaga.com>
2025-03-12 12:51:12 +0100

    puequeña modificacion del getfilename para que este todo corrrecto

--- src/utils/redir_utils.c
+++ src/utils/redir_utils.c
@@ -6,7 +6,7 @@
 /*   By: rmarin-j <rmarin-j@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 17:21:09 by rmarin-j          #+#    #+#             */
-/*   Updated: 2025/03/11 16:23:37 by rmarin-j         ###   ########.fr       */
+/*   Updated: 2025/03/12 12:49:57 by rmarin-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 /*Esta funcion solo coge la siguiente palabra despues
 de la redireccion, q tiene q ser el archivo al q hace ref*/
-
 char	*getfilename(char *str, int i, t_redir *rd)
 {
 	int	start;
@@ -26,37 +25,30 @@ char	*getfilename(char *str, int i, t_redir *rd)
 	if (!str[i] || str[i] == '<' || str[i] == '>')
 		return (NULL);
 	else if (str[i] == '\"')
-	{
 		i = end_quote(str, i + 1, '\"');
-		rd->end_in = i;
-		return (ft_substr(str, start, i - start));
-	}
 	else if (str[i] == '\'')
-	{
 		i = end_quote(str, i + 1, '\'');
-		rd->end_in = i;
-		return (ft_substr(str, start, i - start));
-	}
 	else
 	{
 		while (!ft_isspace(str[i]) && str[i])
 			i++;
-		rd->end_in = i;
-		return (ft_substr(str, start, i - start));
 	}
+	rd->end_in = i;
+	return (ft_substr(str, start, i - start));
 }
 
+
+/*Esta ft añade al final  de la lista de redir un nuevo nodo o lo crea si esta vacia la lista*/
 void	ft_rediradd_back(t_redir **lst, t_redir *new)
 {
-	t_redir * current;
+	t_redir	*current;
 
 	current = *lst;
-
-	if (!*lst) {
+	if (!*lst)
+	{
 		*lst = new;
 		return;
 	}
-
 	while (current && current->next)
 		current = current->next;
 	current->next = new;
