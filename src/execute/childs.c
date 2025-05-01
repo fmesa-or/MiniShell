f8b81f00d5373747c53e0cb0037627730898257c
fmesa-or <franciscomesaempleo@gmail.com>
2025-05-01 12:28:36 +0200

    Pipes 100% DONE

--- src/execute/childs.c
+++ src/execute/childs.c
@@ -6,7 +6,7 @@
 /*   By: fmesa-or <fmesa-or@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 11:17:17 by fmesa-or          #+#    #+#             */
-/*   Updated: 2025/04/30 15:13:31 by fmesa-or         ###   ########.fr       */
+/*   Updated: 2025/05/01 12:02:18 by fmesa-or         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,17 +55,19 @@ void	ms_exe_childs(t_token *token, t_data *data, int fd[2], int fd_in)
 		dup2(data->file_in, STDIN_FILENO);
 	else if (fd_in != STDIN_FILENO)
 		dup2(fd_in, STDIN_FILENO);
-	if (token[1].type == CMD)
+	if (data->typeout == DOUT || data->typeout == NDOUT)
+		dup2(data->file_out, STDOUT_FILENO);
+	else if (token[1].type == CMD)
 		dup2(fd[1], STDOUT_FILENO);
-	dprintf(2, "exe_childs command = %s\n", token->command);//checker
+//	dprintf(2, "exe_childs command = %s\n", token->command);//checker
 //	if (token->fd[0] != 0)
 //		close(token->fd[0]);
 //	if (token->fd[1] != 1)
 //		close(token->fd[1]);
 	close(fd[0]);
 	close(fd[1]);
-	dprintf(2, CI"STDIN: %d || STDOUT: %d\n", STDIN_FILENO, STDOUT_FILENO);
-	dprintf(2, "EXECUTE CHILDS: %s REDIR: fd[0]:%d fd[1]:%d\n"RES, token->command, token->fd[0], token->fd[1]);
+//	dprintf(2, CI"STDIN: %d || STDOUT: %d\n", STDIN_FILENO, STDOUT_FILENO);
+//	dprintf(2, "EXECUTE CHILDS: %s REDIR: fd[0]:%d fd[1]:%d\n"RES, token->command, token->fd[0], token->fd[1]);
 	if (execve(token->command, token->argv, ms_return_env(data)) == -1)
 		ms_cmd_nf(token->argv[0]);
 	exit(127);
