13a478dc504b8529215b0e749c435497b8008bb6
fmesa-or <franciscomesaempleo@gmail.com>
2025-05-07 16:44:59 +0200

    prompt fixed

--- src/prompt.c
+++ src/prompt.c
@@ -6,7 +6,7 @@
 /*   By: fmesa-or <fmesa-or@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 14:38:10 by fmesa-or          #+#    #+#             */
-/*   Updated: 2025/05/07 14:39:10 by fmesa-or         ###   ########.fr       */
+/*   Updated: 2025/05/07 16:41:41 by fmesa-or         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ char	*ms_prompt(t_data *data)
 	aux = find_key(data->exported_list, "USER");//almacena USER en aux (t_list)
 
 	if (aux != NULL)
-		prompt = ft_strjoin(ft_strdup(aux->value), "@");//añade al prompt el valor(char *) de USER
+		prompt = ft_strjoin(ft_strdup(aux->value), "\033[0;92m@\033[0;96m");//añade al prompt el valor(char *) de USER
 	else
-		prompt = ft_strjoin(ft_strdup("try_harder"), "@");//si USER no existe, es porque te has pasado de listillo
+		prompt = ft_strjoin(ft_strdup("try_harder"), "\033[0;92m@\033[0;96m");//si USER no existe, es porque te has pasado de listillo
 
 	aux = find_key(data->exported_list, "SESSION_MANAGER");//almacena un nuevo t_list, para buscar el ordenador que estamos usando en 42
 	if (aux != NULL)
@@ -44,7 +44,7 @@ char	*ms_prompt(t_data *data)
 	}
 	else
 		char_aux = "harder!!";
-	prompt = ft_strjoin(prompt, ft_strjoin(char_aux, ":"));//añadimos el ordenador al usuario
+	prompt = ft_strjoin(prompt, ft_strjoin(char_aux, "\033[0;92m:\033[0;96m"));//añadimos el ordenador al usuario
 	
 	aux = find_key(data->exported_list, "PWD");//almacena un nuevo t_list, buscando donde nos encontramos
 	if (aux)
@@ -69,6 +69,7 @@ char	*ms_prompt(t_data *data)
 		char_aux = ft_substr(char_aux, start, (i - start));
 		prompt = ft_strjoin(ft_strjoin(prompt, "/"), char_aux);
 	}
-	prompt = ft_strjoin(prompt, "$ ");
+	prompt = ft_strjoin(prompt, "\033[0;92m$\033[0m ");
+	prompt = ft_strjoin("\033[0;96m", prompt);
 	return(prompt);
 }
