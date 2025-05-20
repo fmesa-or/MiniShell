239ea4135aa43d25f87a648fbd2ec00a2649006a
fmesa-or <152217033+fmesa-or@users.noreply.github.com>
2025-05-20 16:59:56 +0200

    Normi

--- src/execute/builtins/env.c
+++ src/execute/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: fmesa-or <fmesa-or@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 14:08:11 by fmesa-or          #+#    #+#             */
-/*   Updated: 2025/05/20 15:01:47 by fmesa-or         ###   ########.fr       */
+/*   Updated: 2025/05/20 16:36:25 by fmesa-or         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,16 +59,15 @@ char	**ms_return_env(t_data *data)
 	return (env);
 }
 
-/*
-*Esta funcion es para sacar el env actual por pantalla;
-*	Convierte nuestra lista en un char ** y lo imprime linea a linea
-*	¡¡Revisar si necesita salto de linea!!
-*/
+/**********************************************
+*Prints the whole enviroment.                 *
+*Turns the list in a char array and prints it.*
+**********************************************/
 int	bi_env(t_data *data, t_token *token)
 {
 	char	**env;
 	int		i;
-	
+
 	env = ms_return_env(data);
 	i = 0;
 	while (env[i])
@@ -83,22 +82,5 @@ int	bi_env(t_data *data, t_token *token)
 		i++;
 	}
 	return (0);
-
-/*  V0.1
-	while (list)
-	{
-		if (write(1, list->key, ft_strlen(list->key)) == -1)
-			return (-1);
-		if (list->value)
-		{
-			if (write(1, "=", 1) == -1)
-				return (-1);
-			if (write(1, list->value, ft_strlen(list->value)) == -1)
-				return (-1);
-		}
-		if (write(1, "\n", 1) == -1)
-			return (-1);
-		list = list->next;
-	}
-	return (0);*/
 }
+
--- src/execute/childs.c
+++ src/execute/childs.c
@@ -6,36 +6,11 @@
 /*   By: fmesa-or <fmesa-or@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 11:17:17 by fmesa-or          #+#    #+#             */
-/*   Updated: 2025/05/13 14:33:00 by fmesa-or         ###   ########.fr       */
+/*   Updated: 2025/05/20 16:40:37 by fmesa-or         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-/*
-char	*ms_find_path(char *argv, t_data *data)
-{
-	char			*try_path;
-	char			*path;
-	char			**dirs;
-	unsigned int	i;
-
-	path = ms_find_var
-
-	return ();
-}
-*/
-
-/*static int	ms_is_path(char *command)
-{
-	if (!command)
-		return (0);
-	if (command[0] == '.' && command[1] == '/')
-		return (1);
-	if (command[0] == '/')
-		return (1);
-	return (0);
-}*/
 
 void	ms_check_permision(char *command, t_token *token)
 {
@@ -44,7 +19,7 @@ void	ms_check_permision(char *command, t_token *token)
 		write(2, "minishell: ", 11);
 		write(2, command, ft_strlen(command));
 		write(2, ": Permission denied\n", 20);
-		token->l_status = 126;//revisar si es necesario ponerlo aquí también, yo diría que no.
+		token->l_status = 126;
 		exit(126);
 	}
 }
@@ -71,7 +46,7 @@ void	ms_exe_childs(t_token *token, t_data *data, int fd[2], int fd_in)
 		dprintf(2, "CHECK: %d\n", data->file_in);
 		ms_check_permision(token->command, token);
 		if (execve(token->command, token->argv, ms_return_env(data)) == -1)
-			token->l_status = ms_cmd_nf(token->argv[0]);//dudas si neecsita devolver este valor.
+			token->l_status = ms_cmd_nf(token->argv[0]);
 	}
 	exit(127);
 }
