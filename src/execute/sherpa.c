c86aa428627979c11a897373f3e181a1b99fa60f
fmesa-or <franciscomesaempleo@gmail.com>
2025-05-28 11:33:44 +0200

    fixed issue with cat/wc/grep redirections error

--- src/execute/sherpa.c
+++ src/execute/sherpa.c
@@ -6,7 +6,7 @@
 /*   By: fmesa-or <fmesa-or@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 16:48:34 by fmesa-or          #+#    #+#             */
-/*   Updated: 2025/05/21 23:36:37 by fmesa-or         ###   ########.fr       */
+/*   Updated: 2025/05/28 11:32:24 by fmesa-or         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ static char	**ms_red_argv(char **argv, char *new)
 		return (NULL);//cambiar por un error
 	while (argv[i])
 		i++;
-	newarray = (char **)malloc(sizeof(char *) * i + 2);
+	newarray = (char **)smalloc(sizeof(char *) * (i + 2));
 	if (!newarray)
 	{
 		throw_error("ERROR: malloc failed in ms_red_argv", NULL, NULL);
